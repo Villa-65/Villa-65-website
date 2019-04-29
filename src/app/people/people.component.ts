701fdab628d2686c92aebe7e1d45ecb384cd4b7d
Luc <timmerman.luc1999@gmail.com>
2019-04-29 16:01:14 +0200

    Made people an array.
    Looks nicer and is easier to adjust in the future.

--- src/app/people/people.component.ts
+++ src/app/people/people.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PEOPLE } from '../people';
 
 @Component({
   selector: 'app-people',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./people.component.css']
 })
 export class PeopleComponent implements OnInit {
+  people = PEOPLE;
 
   constructor() { }
 
