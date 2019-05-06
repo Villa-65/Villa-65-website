6eb9a7706f725800957f6e9cf6892ea63702e218
Luctia <timmerman.luc1999@gmail.com>
2019-05-06 21:10:35 +0200

    Working commit for the events component

--- src/app/events/events.component.ts
+++ src/app/events/events.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {GalleryService} from "../gallery.service";
 
 @Component({
   selector: 'app-events',
@@ -6,11 +7,44 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./events.component.css']
 })
 export class EventsComponent implements OnInit {
+  tripPaths: string[];
+  tripsFiles: string[];
+  firstTrip: string;
+  traditionPaths: string[];
+  firstTradition: string;
 
-  constructor() {
+  constructor(
+    private galleryService: GalleryService) {
   }
 
   ngOnInit() {
+    this.getTrips();
+    this.getTraditions();
   }
 
+  private getTraditions() {
+    this.galleryService.getFiles('events/traditions').subscribe(paths => {
+      this.traditionPaths = paths;
+      console.log(this.traditionPaths);
+      // this.firstTradition = this.traditionPaths.splice(0, 1)[0];
+      // console.log(this.traditionPaths);
+      // console.log(this.firstTradition);
+    });
+  }
+
+  private getTrips() {
+    this.galleryService.getFiles('events/trips/thumbs').subscribe(paths => {
+      this.tripsFiles = paths;
+      this.firstTrip = this.tripsFiles.splice(0, 1)[0];
+      for (let i in this.tripsFiles) {
+        this.tripPaths.push(this.getPath(i));
+      }
+    });
+  }
+
+  public getPath(file: string) {
+    file.slice(0, file.search('[.]'));
+    file.replace('/thumbs', '');
+    return file;
+  }
 }
