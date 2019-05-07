588955758da14698654217c144e47eae148f5fc1
Luc <timmerman.luc1999@gmail.com>
2019-05-07 16:35:20 +0200

    Changed stories

--- src/app/people.ts
+++ src/app/people.ts
@@ -1,25 +1,20 @@
 import {Person} from './person';
 
 export const PEOPLE: Person[] = [
-  {id: 0, name: 'Vits', story: 'Ik ben Vits', image: '/people/Vits.png'},
-  {id: 1, name: 'Larissa', story: 'Ik ben Larissa', image: '/people/Larissa.png'},
+  {id: 0, name: 'Vits', story: '', image: '/people/Vits.png'},
+  {id: 1, name: 'Larissa', story: '', image: '/people/Larissa.png'},
   {id: 2, name: '-Leeg-', story: 'Kom hier wonen!', image: '/people/-Leeg-.png'},
-  {id: 3, name: 'Brasser', story: 'Ik ben Brasser', image: '/people/Brasser.png'},
-  {id: 4, name: 'Rosan', story: 'Ik ben Rosan', image: '/people/Rosan.png'},
+  {id: 3, name: 'Brasser', story: '', image: '/people/Brasser.png'},
+  {id: 4, name: 'Rosan', story: '', image: '/people/Rosan.png'},
   {id: 5, name: 'Willy', story: 'Deze kutfeut verdient geen verhaal.', image: '/people/Willy.png'},
-  {id: 6, name: 'Thomas', story: 'Ik ben Thomas', image: '/people/Thomas.png'},
-  {id: 7, name: 'Jari', story: 'Ik ben Jari', image: '/people/Jari.png'},
+  {id: 6, name: 'Thomas', story: '', image: '/people/Thomas.png'},
+  {id: 7, name: 'Jari', story: '', image: '/people/Jari.png'},
   {id: 8, name: '-Leeg-', story: 'Kom hier wonen!', image: '/people/-Leeg-.png'},
-  {
-    id: 9,
-    name: 'Luc',
-    story: 'Hoi, ik ben Luc, de huis-ICT\'er. Ik hou de website en de huiscomputer in de gaten en af en toe maak ik wat leuks, zoals de bierstrepen-applicatie die op de huisPC draait.',
-    image: '/people/Luc.png'
-  },
+  {id: 9, name: 'Luc', story: '', image: '/people/Luc.png'},
   {id: 10, name: 'Quintus', story: 'Deze kutfeut verdient geen verhaal.', image: '/people/Quintus.png'},
   {id: 11, name: '-Leeg-', story: 'Kom hier wonen!', image: '/people/-Leeg-.png'},
-  {id: 12, name: 'Matthijs', story: 'Ik ben Matthijs', image: '/people/Matthijs.png'},
-  {id: 13, name: 'Rick', story: 'Ik ben Rick', image: '/people/Rick.png'},
-  {id: 14, name: 'Arousha', story: 'Ik ben Arousha', image: '/people/Arousha.png'},
-  {id: 15, name: 'Max', story: 'Ik ben Max', image: '/people/Max.png'}
+  {id: 12, name: 'Matthijs', story: '', image: '/people/Matthijs.png'},
+  {id: 13, name: 'Rick', story: '', image: '/people/Rick.png'},
+  {id: 14, name: 'Arousha', story: '', image: '/people/Arousha.png'},
+  {id: 15, name: 'Max', story: '', image: '/people/Max.png'}
 ];
