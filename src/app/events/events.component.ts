7f4a7d83f24b31a9c5e215ebd7b502e9939e3ab5
Luctia <timmerman.luc1999@gmail.com>
2019-05-07 14:20:53 +0200

    Fixed events page. Now works perfectly
    Only have to fix some style components

--- src/app/events/events.component.ts
+++ src/app/events/events.component.ts
@@ -7,11 +7,8 @@ import {GalleryService} from "../gallery.service";
   styleUrls: ['./events.component.css']
 })
 export class EventsComponent implements OnInit {
-  tripPaths: string[];
   tripsFiles: string[];
-  firstTrip: string;
   traditionPaths: string[];
-  firstTradition: string;
 
   constructor(
     private galleryService: GalleryService) {
@@ -25,26 +22,18 @@ export class EventsComponent implements OnInit {
   private getTraditions() {
     this.galleryService.getFiles('events/traditions').subscribe(paths => {
       this.traditionPaths = paths;
-      console.log(this.traditionPaths);
-      // this.firstTradition = this.traditionPaths.splice(0, 1)[0];
-      // console.log(this.traditionPaths);
-      // console.log(this.firstTradition);
     });
   }
 
   private getTrips() {
     this.galleryService.getFiles('events/trips/thumbs').subscribe(paths => {
       this.tripsFiles = paths;
-      this.firstTrip = this.tripsFiles.splice(0, 1)[0];
-      for (let i in this.tripsFiles) {
-        this.tripPaths.push(this.getPath(i));
-      }
     });
   }
 
-  public getPath(file: string) {
-    file.slice(0, file.search('[.]'));
-    file.replace('/thumbs', '');
+  public getLink(file: string) {
+    file = file.slice(0, file.search("[.]"));
+    file = file.replace("events/trips/thumbs", "/events");
     return file;
   }
 }
