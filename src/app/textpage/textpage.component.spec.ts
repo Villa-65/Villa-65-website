549e143bae56a35b4a13ccace8da1ee8564f09de
luctia <timmerman.luc1999@gmail.com>
2021-03-06 19:24:07 +0100

    Update naar 11

--- src/app/textpage/textpage.component.spec.ts
+++ src/app/textpage/textpage.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { TextpageComponent } from './textpage.component';
 
@@ -6,7 +6,7 @@ describe('TextpageComponent', () => {
   let component: TextpageComponent;
   let fixture: ComponentFixture<TextpageComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ TextpageComponent ]
     })
